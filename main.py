ac3ba9bdaa01fe6601e20a4c58dd79a3653f2967
Jonathan Mani <39526978+jonathanmani@users.noreply.github.com>
2023-09-01 14:43:38 -0400

    Set up code to take input from user

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 
 def main():
-    book, author, release_year, goodreads_rating = 'Dracula', 'Brom Stoker', 1897, 4.01
+    name = input("What's your name?")
+    age = int(input(f'How old are you {name}?'))
+    year = int(input('What year is it?'))
 
-    print(f'{book} is by {author} and it was realeased in {release_year}. It has a rating of {goodreads_rating} on goodreads')
+    print(f'So {name}, that means you were born in {year - age}!')
+
+
 if __name__ =='__main__':
     main()
