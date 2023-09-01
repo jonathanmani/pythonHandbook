7f680de5bcc064b55d241b64ff1ed1aebe40f83b
Jonathan Mani <39526978+jonathanmani@users.noreply.github.com>
2023-09-01 15:29:54 -0400

    Used nested for loops to create multiplication tables

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 
 def main():
-    name = input("What's your name?")
-    age = int(input(f'How old are you {name}?'))
-    year = int(input('What year is it?'))
-
-    print(f'So {name}, that means you were born in {year - age}!')
+    for numbers in range(10):
+        for nums in range(10):
+            print(f'{numbers} * {nums} = {numbers * nums}')
 
 
 if __name__ =='__main__':
