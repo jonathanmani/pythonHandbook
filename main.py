e1e8da5edfb2d6dede530f7c3d4b0510b5a6081f
Jonathan Mani <39526978+jonathanmani@users.noreply.github.com>
2023-09-01 14:00:11 -0400

    Changed the sentence in print and used f strings

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 def main():
-    print("Hello World")
+    book, author, release_year, goodreads_rating = 'Dracula', 'Brom Stoker', 1897, 4.01
 
+    print(f'{book} is by {author} and it was realeased in {release_year}. It has a rating of {goodreads_rating} on goodreads')
 if __name__ =='__main__':
     main()
